6a075b2c86be5f6bf7edcc0b3d0e86eb271e4e7f
Drillpap1 <melvin.lawal@hotmail.de>
2025-04-15 20:54:28 +0200

    Move copyright text directly below footer logo

--- MALER_THEME/footer.php
+++ MALER_THEME/footer.php
@@ -12,9 +12,23 @@
     <footer id="colophon" class="site-footer">
         <div class="container">
             <div class="footer-content">
-                <!-- Footer Logo -->
-                <div class="footer-logo">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/footer_logo.png" alt="Malermeister Philip Krzywinski Logo">
+                <!-- Footer Logo und Copyright -->
+                <div class="footer-logo-container">
+                    <div class="footer-logo">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/footer_logo.png" alt="Malermeister Philip Krzywinski Logo">
+                    </div>
+                    <div class="copyright-text">
+                        <p>
+                            <?php
+                            printf(
+                                /* translators: %1$s: Jahr, %2$s: Site-Name */
+                                esc_html__('© %1$s %2$s', 'maler-theme'),
+                                '2025',
+                                'Malermeister Philip Krzywinski'
+                            );
+                            ?>
+                        </p>
+                    </div>
                 </div>
 
                 <!-- Adresse -->
@@ -36,20 +50,6 @@
                     </ul>
                 </div>
             </div>
-        </div>
-            
-        <!-- Copyright-Information -->
-        <div class="site-info">
-            <p>
-                <?php
-                printf(
-                    /* translators: %1$s: Jahr, %2$s: Site-Name */
-                    esc_html__('© %1$s %2$s', 'maler-theme'),
-                    '2025',
-                    'Malermeister Philip Krzywinski'
-                );
-                ?>
-            </p>
         </div>
     </footer>
 </div><!-- #page -->
